3895b674a012fde36086d157204a9ec5c45dad92
jenniferDelgadoLozano <117748187+jenniferDelgadoLozano@users.noreply.github.com>
2023-11-02 07:44:47 -0500

    Add files via upload
    layoud

---
+++ admin_menu.php
@@ -0,0 +1,73 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Panel de control</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle2">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>Accesos</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Administrar grupos</a></li>
+      <li><a href="users.php">Administrar usuarios</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Media</span>
+    </a>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categorías</span>
+    </a>
+  </li>
+  <li>
+    <a href="ubicacion.php" >
+      <i class="glyphicon glyphicon-map-marker"></i>
+      <span>Bodegas</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle2">
+      <i class="glyphicon glyphicon-arrow-left"></i>
+      <span>Entradas</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="add_product.php">Agregar entrada</a></li>
+       <li><a href="product.php">Administrar entradas</a></li>
+   </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle2">
+      <i class="glyphicon glyphicon-arrow-right"></i>
+      <span>Salidas</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales.php">Administrar Salidas</a></li>
+      <li><a href="add_sale.php">Agregar salida</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="actas.php">
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Actas</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle2">
+      <i class="glyphicon glyphicon-signal"></i>
+      <span>Reportes</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales_report.php">salidas por fecha</a></li>
+      <li><a href="entradas_report.php">entradas por fecha</a></li>
+      <li><a href="kardex.php">Kardex</a></li>
+    </ul>
+  </li>
+</ul>
